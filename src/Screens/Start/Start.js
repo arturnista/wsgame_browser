f5ac87ed4be6d4a47cf416b23c633dabdd4fd312
Artur Nista <arturnista@gmail.com>
2018-06-07 13:48:40 -0300

    Room list

--- src/Screens/Start/Start.js
+++ src/Screens/Start/Start.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Input, Button } from '../../Components'
+import { serverUrl } from '../../constants'
 import _ from 'lodash'
 import moment from 'moment'
 import './Start.css'
@@ -20,11 +21,18 @@ class Start extends Component {
 
         this.state = {
             userName: 'Good user name',
-            roomName: 'A very good room name'
+            roomName: 'A very good room name',
+            rooms: []
         }
 
         this._handleCreateRoom = this._handleCreateRoom.bind(this)
         this._handleJoinRoom = this._handleJoinRoom.bind(this)
+        this.handleRefresh = this.handleRefresh.bind(this)
+        this.renderRoomLine = this.renderRoomLine.bind(this)
+    }
+
+    componentDidMount() {
+        this.handleRefresh()
     }
 
     componentWillReceiveProps(nextProps) {
@@ -33,9 +41,17 @@ class Start extends Component {
         }
     }
 
-    _handleJoinRoom() {
+    handleRefresh() {
+        fetch(`${serverUrl}/rooms`)
+        .then(rooms => rooms.json())
+        .then(rooms => {
+            this.setState({ rooms })
+        })
+    }
+
+    _handleJoinRoom(name) {
         this.setState({ roomJoinedIsOwner: false })
-        window.socketio.emit('room_join', { name: this.state.roomName, userName: this.state.userName })
+        window.socketio.emit('room_join', { name, userName: this.state.userName })
     }
 
     _handleCreateRoom() {
@@ -43,28 +59,67 @@ class Start extends Component {
         window.socketio.emit('room_create', { name: this.state.roomName, userName: this.state.userName })
     }
 
+    renderRoomLine(room) {
+
+        return (
+
+            <div className='start-room-container'>
+                <div className='start-room-name-container'>
+                    <h2 className='start-room-name'>{room.name}</h2>
+                    <p className='start-room-owner'>Owner: {room.owner.name}</p>
+                </div>
+                <div className='start-room-players-container'>
+                    <p className='start-room-players-title'>Players</p>
+                    <p className='start-room-players-count'>{room.users.length}</p>
+                </div>
+                <div className='start-room-action-container'>
+                    <Button label='Join' className='start-room-button-enter'
+                        onClick={() => this._handleJoinRoom(room.name)}
+                    />
+                </div>
+            </div>
+
+        )
+    }
+
     render() {
 
         return (
             <div className="start-container">
-                <div className="start-user-container">
-                    <h2 className="start-room-title">User configuration</h2>
-                    <Input label='Name' className='start-input-username'
-                        placeholder='Robson'
-                        value={this.state.userName}
-                        onChange={x => this.setState({ userName: x })} />
+                <div className='start-side-container'>
+                    <h2 className="start-room-conf-title start-rooms-list-title">Rooms</h2>
+                    <div className='start-rooms-list-refresh-container'
+                        onClick={this.handleRefresh}>
+                        <p className='start-rooms-list-refresh-help'>Press to refresh</p>
+                    </div>
+                    <div className='start-rooms-list'>
+                        { this.state.rooms.map(this.renderRoomLine) }
+                    </div>
                 </div>
-                <div className="start-room-container">
-                    <h2 className="start-room-title">Room configuration</h2>
-                    <Input label='Room name' className='start-input-roomname'
-                        placeholder="Robson's room"
-                        value={this.state.roomName}
-                        onChange={x => this.setState({ roomName: x })} />
-                    <div className="start-room-buttons">
-                        <Button label='Create' className='start-button left'
-                            onClick={this._handleCreateRoom}/>
-                        <Button label='Enter' className='start-button right'
-                            onClick={this._handleJoinRoom}/>
+                <div className='start-side-container'>
+                    <div className="start-user-container">
+                        <h2 className="start-room-conf-title">User configuration</h2>
+                        <Input label='Name' className='start-input-username'
+                            placeholder='Robson'
+                            value={this.state.userName}
+                            onChange={x => this.setState({ userName: x })}
+                        />
+                    </div>
+                    <div className="start-room-conf-container">
+                        <h2 className="start-room-conf-title">Room configuration</h2>
+                        <Input label='Room name' className='start-room-conf-input-roomname'
+                            placeholder="Robson's room"
+                            value={this.state.roomName}
+                            onChange={x => this.setState({ roomName: x })}
+                        />
+                        <div className="start-room-conf-buttons">
+                            <Button label='Create' className='start-button left'
+                                onClick={this._handleCreateRoom}
+                            />
+                            <Button label='Enter' className='start-button right'
+                                onClick={() => this._handleJoinRoom(this.state.roomName)}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
