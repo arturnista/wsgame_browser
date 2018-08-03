c7c902e498657250425f91c4a2ec0bff30277ece
Artur Nista <arturnista@gmail.com>
2018-08-03 18:00:15 -0300

    Prison

--- src/Screens/Game/Spells/Prison.js
+++ src/Screens/Game/Spells/Prison.js
@@ -4,7 +4,6 @@ import vector from '../../../Utils/vector'
 export function createPrison(spellData) {
     const spell = new window.PIXI.Sprite( window.textures['prison.png'] )
     spell.anchor.set(.5, .5)
-    console.log('spellData', spellData);
     
     spell.id = spellData.id
 
