13a26b06e8c124bd5723e28613965f021d789caa
Artur <arturnista@gmail.com>
2018-08-27 19:41:24 -0300

    v0.4.0

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,7 +11,7 @@ class Header extends Component {
             <div className="header-container">
                 <Link to='/' className='header-title'>
                     <h1>NW GAME</h1>
-                    <small>v0.3.1</small>
+                    <small>v0.4.0</small>
                 </Link>
                 <Link to='/' className={`header-link ${ this.props.location.pathname === '/' ? 'active' : ''} `}>
                     <p>Home</p>
