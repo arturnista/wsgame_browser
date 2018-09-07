0246e94d72d4e0a692488310c0f22eb9524a8e92
Artur <arturnista@gmail.com>
2018-09-07 19:58:04 -0300

    Removed useless actions

--- src/Redux/user.js
+++ src/Redux/user.js
@@ -1,9 +1,6 @@
 const DEFINE_USER = 'user/DEFINE_USER'
 const ADD_PREFERENCES = 'user/ADD_PREFERENCES'
 const USER_END_GAME = 'user/USER_END_GAME'
-const SET_SPELLS = 'user/SET_SPELLS'
-const SELECT_SPELL = 'user/SELECT_SPELL'
-const DESELECT_SPELL = 'user/DESELECT_SPELL'
 
 const initialState = null
 
@@ -29,29 +26,6 @@ export default function reducer(state = initialState, action = {}) {
             }
 
             return newPreference
-        case SET_SPELLS:
-            return {
-                ...state,
-                spells: [ ...action.payload ]
-            }
-        case SELECT_SPELL:
-            const nSelectSpells = state.spells.filter(x => x.position !== action.payload.position)
-            return {
-                ...state,
-                spells: [
-                    ...nSelectSpells,
-                    {
-                        hotkey: action.payload.hotkey,
-                        position: action.payload.position,
-                        id: action.payload.id,
-                    }
-                ]
-            }
-        case DESELECT_SPELL:
-            return {
-                ...state,
-                spells: state.spells.filter(x => x.id !== action.payload.id)
-            }
         default:
             return state
     }
@@ -70,36 +44,3 @@ export function addUserPreferences(prefs) {
         payload: prefs
     }
 }
-
-export function setSpells(spells, hotkeysConfig) {
-    return {
-        type: SET_SPELLS,
-        payload: spells.map(spell => ({
-            id: spell.id,
-            position: spell.position,
-            hotkey: hotkeysConfig[spell.position]
-        }))
-    }
-}
-
-export function selectSpell(id, position, hotkeysConfig) {
-    return {
-        type: SELECT_SPELL,
-        payload: {
-            id,
-            position,
-            hotkey: hotkeysConfig[position]
-        }
-    }
-}
-
-export function deselectSpell(id, position) {
-    return {
-        type: DESELECT_SPELL,
-        payload: {
-            id,
-            position
-        }
-    }
-}
-
